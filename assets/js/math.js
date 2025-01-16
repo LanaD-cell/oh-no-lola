090c0516544c3ae2fc9f84f173358ee748998d3e
Illana de <rrwycmqu@students.codeinstitute.net>
2025-01-16 14:25:01 +0000

    create a js file dedicated to math page only

---
+++ assets/js/math.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let mbutton = document.getElementById("math-btn");
+  
+    for (let i = 0; i > mbutton.length; i++) {
+      button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "submit") {
+          alert("You clicked Submit!");
+        } else {
+  let gameType = this.getAttribute("data-type");
+  alert(`You clicked ${gameType}`);
+        }
+      })
+  
+    }
+  })
+  function runGame() {
+  
+  }
+  
+  function checkAnswer() {
+  
+  }
+  
+  function calculateCorrectAnswer() {
+  
+  }
+  
+  function incrementScore() {
+  
+  }
+  
+  function incrementWrongAnswer() {
+  
+  }
+  
+  function displayAdditionQuestion() {
+  
+  }
+  
+  function displaySubtractQuestion() {
+  
+  }
+  
+  function displayMultiplyQuestion() {
+    
+  }
