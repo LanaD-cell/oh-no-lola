7634f92ccf6715dd05e36f5a75c1e9d20764ddc4
Illana de <rrwycmqu@students.codeinstitute.net>
2025-01-21 15:11:50 +0000

    fix errors js

--- assets/js/bubbles.js
+++ assets/js/bubbles.js
@@ -1,31 +1,47 @@
-const cards = document.querySelectorAll(".flipcard");
+const cards = document.querySelectorAll(".card-game");
 
-let flipcardOne, flipcardTwo;
+let cardOne, cardTwo;
 
 // define flipCard function
 function flipCard(e) {
     let clickedCard = e.target; // getting clicked card
     clickedCard.classList.add("flip");
-    if (clickedCard !== flipcardOne) {
-        if (!flipcardOne) {
+    if (clickedCard !== cardOne) {
+        if (!cardOne) {
             //cardOne value returned
-            return flipcardOne = clickedCard;
+            return cardOne = clickedCard;
         }
-        flipcardTwo = clickedCard;
+        cardTwo = clickedCard;
 
-        let flipcardOneImg = flipcardOne.querySelector("img").src,
-            flipcardTwoImg = flipcardTwo.querySelector("img").src;
-        matchCards(flipcardOneImg, flipcardTwoImg);
+        let cardOneImg = cardOne.querySelector(".card-game img").src,
+            cardTwoImg = cardTwo.querySelector(".card-game img").src;
+        matchCards(cardOneImg, cardTwoImg);
     }
 }
 
 function matchCards(img1, img2) {
     if (img1 === img2) { // the two img matched
-        return console.log("Card matched");
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";//card values to blank
+        return;
     }
+
+// if no match, shake after 400ms
+setTimeout(() => {
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+}, 400);
+
+// remove shake after 1200ms
+setTimeout(() => {
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = "";//card values to blank
+}, 1200);
 }
 
 // add click efect to card
-cards.forEach(flipcard => {
-    flipcard.addEventListener("click", flipCard);
+cards.forEach(card => {
+    card.addEventListener("click", flipCard);
 });
