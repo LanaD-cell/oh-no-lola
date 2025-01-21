60e95d01bead4c198b19747f06148c8602eb7776
Illana de <rrwycmqu@students.codeinstitute.net>
2025-01-21 17:11:22 +0000

    add styling to flip game

--- assets/js/bubbles.js
+++ assets/js/bubbles.js
@@ -23,25 +23,13 @@ function matchCards(img1, img2) {
     if (img1 === img2) { // the two img matched
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
-        cardOne = cardTwo = "";//card values to blank
+        cardOne = cardTwo = ""; //card values to blank
         return;
     }
 
-// if no match, shake after 400ms
-setTimeout(() => {
-    cardOne.classList.add("shake");
-    cardTwo.classList.add("shake");
-}, 400);
 
-// remove shake after 1200ms
-setTimeout(() => {
-    cardOne.classList.remove("shake", "flip");
-    cardTwo.classList.remove("shake", "flip");
-    cardOne = cardTwo = "";//card values to blank
-}, 1200);
-}
-
-// add click efect to card
-cards.forEach(card => {
-    card.addEventListener("click", flipCard);
-});
+    // add click efect to card
+    cards.forEach(card => {
+        card.addEventListener("click", flipCard);
+    });
+    
