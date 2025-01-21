b0b62e290e12d17ddf3c342647be69c614f18c9b
Illana de <rrwycmqu@students.codeinstitute.net>
2025-01-21 11:02:23 +0000

    work on flip game

--- assets/js/bubbles.js
+++ assets/js/bubbles.js
@@ -1,62 +1,34 @@
-const cards = document.querySelectorAll(".card");
-let matched = 0;
+const cards = document.querySelectorAll(".flipcard");
+
 let cardOne, cardTwo;
-let disableDeck = false;
-function flipCard({target: clickedCard}) {
-    if(cardOne !== clickedCard && !disableDeck) {
-        clickedCard.classList.add("flip");
-        if(!cardOne) {
+
+// define flipCard function
+function flipCard(e) {
+    let clickedCard = e.target; // getting clicked card
+    clickedCard.classList.add("flip");
+    if (clickedCard !== cardOne) {
+        if (!cardOne) {
+            //cardOne value returned
             return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
-        disableDeck = true;
-        let cardOneImg = cardOne.querySelector(".back-view img").src,
-        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+
+        let cardOneImg = cardOne.querySelector("img").src,
+            cardTwoImg = cardTwo.querySelector("img").src;
         matchCards(cardOneImg, cardTwoImg);
     }
 }
-function matchCards(img1, img2) {
-    if(img1 === img2) {
-        matched++;
-        if(matched == 8) {
-            setTimeout(() => {
-                return shuffleCard();
-            }, 1000);
-        }
-        cardOne.removeEventListener("click", flipCard);
-        cardTwo.removeEventListener("click", flipCard);
-        cardOne = cardTwo = "";
-        return disableDeck = false;
-    }
-    setTimeout(() => {
-        cardOne.classList.add("shake");
-        cardTwo.classList.add("shake");
-    }, 400);
-    setTimeout(() => {
-        cardOne.classList.remove("shake", "flip");
-        cardTwo.classList.remove("shake", "flip");
-        cardOne = cardTwo = "";
-        disableDeck = false;
-    }, 1200);
-}
-function shuffleCard() {
-    matched = 0;
-    disableDeck = false;
-    cardOne = cardTwo = "";
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
-    cards.forEach((card, i) => {
-        card.classList.remove("flip");
-        let imgTag = card.querySelector(".back-view img");
-        imgTag.src = `images/img-${arr[i]}.png`;
-        card.addEventListener("click", flipCard);
-    });
-}
-shuffleCard();
-    
-cards.forEach(card => {
-    card.addEventListener("click", flipCard);
-});
 
-// take user back to top
-const toTop = () => window.scrollTo({top:0, behaviour: 'smooth'});
+function matchCards(img1, img2) {
+    if (img1 === img2) { // the two img matched
+        return console.log("Card matched");
+    }
+    
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+}
+
+// add click efect to card
+cards.forEach(flipcard => {
+    flipcard.addEventListener("click", flipCard);
+});
