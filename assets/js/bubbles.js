b88ab7082ed229541c1cec0d9215d2246bdf14ed
Illana de <rrwycmqu@students.codeinstitute.net>
2025-01-21 18:17:01 +0000

    create random shuffle for cards

--- assets/js/bubbles.js
+++ assets/js/bubbles.js
@@ -26,39 +26,49 @@ function matchCards(img1, img2) {
         matchedCard++; //matched value +1
         // matched value is 8, the all cards matched (8*2 = 16)
         if (matchedCard == 8) {
-            shuffleCard();
-        }
-        cardOne.removeEventListener("click", flipCard);
-        cardTwo.removeEventListener("click", flipCard);
-        cardOne = cardTwo = ""; //card values to blank
-        return disableDeck = false;
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000); //call shuffle after 1s
     }
+    cardOne.removeEventListener("click", flipCard);
+    cardTwo.removeEventListener("click", flipCard);
+    cardOne = cardTwo = ""; //card values to blank
+    return disableDeck = false;
+}
 
-    setTimeout(() => {
-        // when 2 cards not matched
-        cardOne.classList.add("shake");
-        cardTwo.classList.add("shake");
-    }, 400); // add shake class to both cards after 400ms
+setTimeout(() => {
+    // when 2 cards not matched
+    cardOne.classList.add("shake");
+    cardTwo.classList.add("shake");
+}, 400); // add shake class to both cards after 400ms
 
-    setTimeout(() => {
-        // when 2 cards not matched
-        cardOne.classList.remove("shake", "flip");
-        cardTwo.classList.remove("shake", "flip");
-        cardOne = cardTwo = ""; //reser both values to blank
-        disableDeck = false;
-    }, 1200); // remove shake and flip class to both cards after 1.2s
+setTimeout(() => {
+    // when 2 cards not matched
+    cardOne.classList.remove("shake", "flip");
+    cardTwo.classList.remove("shake", "flip");
+    cardOne = cardTwo = ""; //reser both values to blank
+    disableDeck = false;
+}, 1200); // remove shake and flip class to both cards after 1.2s
 }
 
 
 function shuffleCard() {
-    matchedCard = 0; 
-    cardOne = cardTwo ? "";
-    cards.forEach(card => {
-        card.classList.remove("flip";)
+    matchedCard = 0;
+    cardOne = cardTwo = "";
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8,];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1); //sort array randomly
+   
+    // remove flip class for all cards and pass random image to each card
+    cards.forEach((card, index) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector("img");
+        imgTag.src = `assets/images/game-icons/${arr[index]}.png`;
         card.addEventListener("click", flipCard);
     });
 }
+
 // add click efect to card
 cards.forEach(card => {
+    card.classList.add("flip");
     card.addEventListener("click", flipCard);
 });
